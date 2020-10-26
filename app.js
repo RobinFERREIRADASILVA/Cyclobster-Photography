8d526936c519fef8663e671e0d01279ae7d942ea
Robin FERREIRA DA SILVA <ferreiradasilvarobin@gmail.com>
2020-10-26 19:55:31 +0100

    Début de la navbar mobile

---
+++ app.js
@@ -0,0 +1,33 @@
+let app = {
+
+    hamburgerElement : null,
+    closeElement : null,
+    mobileExpand : null,
+    mobileNav : null,
+    
+   init: function() {
+        app.hamburgerElement = document.querySelector('.hamburger');
+        app.mobileExpand = document.querySelector('#mobile-expand');
+        app.mobileNav = document.querySelector('#mobile-nav');
+        app.closeElement = document.querySelector('.fa-times');
+
+        app.hamburgerElement.addEventListener('click', app.handleOpenNavBar);
+        app.closeElement.addEventListener('click', app.handleCloseNavBar)
+    },
+
+    handleOpenNavBar : function(evt)
+    {
+           app.mobileExpand.classList.remove('displayNone');
+           app.mobileNav.classList.add('displayNone');
+
+    },
+
+    handleCloseNavBar : function(evt)
+    {
+           app.mobileExpand.classList.add('displayNone');
+           app.mobileNav.classList.remove('displayNone');
+    }
+
+}
+
+app.init();
