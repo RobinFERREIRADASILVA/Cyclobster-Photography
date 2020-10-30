e3fbd10d6290e57b2767e8399fff3d2d1e35fef0
Robin FERREIRA DA SILVA <ferreiradasilvarobin@gmail.com>
2020-10-30 23:48:08 +0100

    Galerry version desktop 95%

--- app/views/main/gallery.tpl.php
+++ app/views/main/gallery.tpl.php
@@ -1,31 +1,42 @@
 <div class="gallery-bg">
-    <div class="main-category">
-        <div class="category studio">
-            <div class="wrap">
-                <h2 class="category-title">Studio</h2>
+    
+        <div class="category-top">
+            <div class="category studio">
+                <div class="wrap">
+                    <h2 class="category-title">Studio</h2>
+                    <a href="#" class="btn">View photos</a>
+                </div>
+            </div>
+            <div class="category outdoor">
+                <div class="wrap">
+                    <h2 class="category-title">Outdoor</h2>
+                    <a href="#" class="btn">View photos</a>
+                </div>
             </div>
         </div>
         <div class="category argentique">
-            <h2 class="category-title">Argentique</h2>
+            <div class="wrap">
+                <h2 class="category-title">Argentique</h2>
+                <a href="#" class="btn">View photos</a>
+            </div>
         </div>  
-    </div>
-    <div class="sub-category">
-        <div class="category outdoor">
-            <div class="wrap">
-                <h2 class="category-title">Outdoor</h2>
+
+        <div class="category-bottom">
+            <div class="category nature">
+                <div class="wrap">
+                    <h2 class="category-title">Nature & Découverte</h2>
+                    <a href="#" class="btn">View photos</a>
+                </div>
+            </div>
+            <div class="category lingerie">
+                <div class="wrap">
+                    <h2 class="category-title">Lingerie</h2>
+                    <a href="#" class="btn">View photos</a>
+                </div>
             </div>
         </div>
-        <div class="category nature">
-            <div class="wrap">
-                <h2 class="category-title">Nature & Découverte</h2>
-            </div>
-        </div>
-        <div class="category lingerie">
-            <div class="wrap">
-                <h2 class="category-title">Lingerie</h2>
-            </div>
-        </div>
-    </div>
+
+
         
 
 </div>
