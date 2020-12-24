b713ead31ef986e1bb1ce36990177857e681edee
RobinFERREIRADASILVA <ferreiradasilvarobin@gmail.com>
2020-12-24 12:40:55 +0100

    Fix bug gallery and about

--- app/views/gallery/gallery.tpl.php
+++ app/views/gallery/gallery.tpl.php
@@ -11,12 +11,15 @@
             </div>
             <?php endfor; ?>
         </div>
-        <div class="category background-option middle-margin" style ="background: url('<?= $_SERVER['BASE_URI'] ?>/assets/img/<?= $categoryList[2]->getPicture() ?>');">
-            <div class="wrap">
-                <h2 class="category-title"><?= $categoryList[2]->getName(); ?></h2>
-                <a href="<?= $router->generate('category',['id' =>  $categoryList[2]->getId()]) ?>" class="btn">View category</a>
-            </div>
-        </div>  
+        <div class="category">
+            <div class="category category-middle background-option middle-margin" style ="background: url('<?= $_SERVER['BASE_URI'] ?>/assets/img/<?= $categoryList[2]->getPicture() ?>');">
+                <div class="wrap">
+                    <h2 class="category-title"><?= $categoryList[2]->getName(); ?></h2>
+                    <a href="<?= $router->generate('category',['id' =>  $categoryList[2]->getId()]) ?>" class="btn">View category</a>
+                </div>
+            </div> 
+        </div>
+         
 
         <div class="category-bottom">
         <?php for ($index=3; $index<5; $index++): ?>
